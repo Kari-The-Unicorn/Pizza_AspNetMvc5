5ab036f0da8cc428f4e78ad6b26766e264f16c39
Kari-The-Unicorn <inmatrixos@gmail.com>
2022-03-29 20:13:55 +0100

    added selenium ui browser tests

---
+++ Pizza_AspNetMvc5.UITests/Pizza_WebAppShould.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Xunit;
+
+namespace Pizza_AspNetMvc5.UITests
+{
+	public class Pizza_WebAppShould
+	{
+		[Fact]
+		[Trait("Category", "Smoke")] // if loads without error
+		public void LoadApplicationPage()
+		{
+			using (IWebDriver driver = new ChromeDriver())
+			{
+				const string appUrl = "https://localhost:44303/";
+				driver.Navigate().GoToUrl(appUrl);
+				string pageTitle = driver.Title;
+
+				Assert.Equal(appUrl, driver.Url);
+				Assert.Equal("Home - My Pizzerias App", pageTitle);
+			}
+		}
+	}
+}
