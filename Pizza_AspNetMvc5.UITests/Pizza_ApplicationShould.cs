9c844e4d90ff298d91b90756aedb8d32b7b4ec8c
Kari-The-Unicorn <inmatrixos@gmail.com>
2022-03-30 18:01:32 +0100

    updated selenium ui browser tests

---
+++ Pizza_AspNetMvc5.UITests/Pizza_ApplicationShould.cs
@@ -0,0 +1,67 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Xunit;
+
+namespace Pizza_AspNetMvc5.UITests
+{
+	[Trait("Category", "Applications")]
+	public class Pizza_ApplicationShould
+	{
+		private const string HomeUrl = "https://localhost:44303/";
+		private const string PizzeriasUrl = "https://localhost:44303/Pizzerias";
+		private const string CreateNewUri = "/Create";
+		private const string PizzeriasTitle = "Index - My Pizzerias App";
+		private const string CreateNewTitle = "Create - My Pizzerias App";
+
+		[Fact]
+		public void SeeAllPizzeriasFromHomePage()
+		{
+			using (IWebDriver driver = new ChromeDriver())
+			{
+				driver.Navigate().GoToUrl(HomeUrl);
+				TestHelper.Pause();
+
+				IWebElement seeAllPizzeriasLink = driver.FindElement(By.Name("SeeAllPizzerias"));
+				seeAllPizzeriasLink.Click();
+				TestHelper.Pause();
+
+				Assert.Equal(PizzeriasTitle, driver.Title); 
+				Assert.Equal(PizzeriasUrl, driver.Url);
+			}
+		}
+
+		[Fact]
+		public void SeeAllPizzeriasFromHomePageLogo()
+		{
+			using (IWebDriver driver = new ChromeDriver())
+			{
+				driver.Navigate().GoToUrl(HomeUrl);
+				TestHelper.Pause();
+
+				IWebElement seeAllPizzeriasLink = driver.FindElement(By.Id("allPizzeriasLogo"));
+				seeAllPizzeriasLink.Click();
+				TestHelper.Pause();
+
+				Assert.Equal(PizzeriasTitle, driver.Title);
+				Assert.Equal(PizzeriasUrl, driver.Url);
+			}
+		}
+
+		[Fact]
+		public void SeeCreateNewPizzeriaFromPizzeriasPage()
+		{
+			using (IWebDriver driver = new ChromeDriver())
+			{
+				driver.Navigate().GoToUrl(PizzeriasUrl);
+				TestHelper.Pause();
+
+				IWebElement createNewPizzeriaLink = driver.FindElement(By.LinkText("Add New"));
+				createNewPizzeriaLink.Click();
+				TestHelper.Pause();
+
+				Assert.Equal(CreateNewTitle, driver.Title);
+				Assert.Equal($"{PizzeriasUrl}{CreateNewUri}", driver.Url);
+			}
+		}
+	}
+}
--- Pizza_AspNetMvc5.UITests/Pizza_WebAppShould.cs
+++ Pizza_AspNetMvc5.UITests/Pizza_WebAppShould.cs
@@ -6,18 +6,82 @@ namespace Pizza_AspNetMvc5.UITests
 {
 	public class Pizza_WebAppShould
 	{
+		private const string HomeUrl = "https://localhost:44303/";
+		private const string AboutUrl = "https://localhost:44303/Home/About";
+		private const string HomeTitle = "Home - My Pizzerias App";
+
+		// if loads without error
 		[Fact]
-		[Trait("Category", "Smoke")] // if loads without error
-		public void LoadApplicationPage()
+		[Trait("Category", "Smoke")] 
+		public void LoadHomePage()
 		{
 			using (IWebDriver driver = new ChromeDriver())
 			{
-				const string appUrl = "https://localhost:44303/";
-				driver.Navigate().GoToUrl(appUrl);
+				driver.Navigate().GoToUrl(HomeUrl);
+				TestHelper.Pause();
 				string pageTitle = driver.Title;
 
-				Assert.Equal(appUrl, driver.Url);
-				Assert.Equal("Home - My Pizzerias App", pageTitle);
+				Assert.Equal(HomeUrl, driver.Url);
+				Assert.Equal(HomeTitle, pageTitle);
+			}
+		}
+
+		// if reloads without error
+		[Fact]
+		[Trait("Category", "Smoke")] 
+		public void ReloadHomePage()
+		{
+			using (IWebDriver driver = new ChromeDriver())
+			{
+				driver.Navigate().GoToUrl(HomeUrl);
+				TestHelper.Pause();
+				driver.Navigate().Refresh();
+
+				Assert.Equal(HomeUrl, driver.Url);
+				Assert.Equal(HomeTitle, driver.Title);
+			}
+		}
+
+		// if reloads home page when going to different url and clicking back
+		[Fact]
+		[Trait("Category", "Smoke")] 
+		public void ReloadHomePageOnBack()
+		{
+			using (IWebDriver driver = new ChromeDriver())
+			{
+				driver.Navigate().GoToUrl(HomeUrl);
+				TestHelper.Pause();
+				driver.Navigate().GoToUrl(AboutUrl);
+				TestHelper.Pause();
+				driver.Navigate().Back();
+				TestHelper.Pause();
+
+				Assert.Equal(HomeUrl, driver.Url);
+				Assert.Equal(HomeTitle, driver.Title);
+			}
+		}
+
+		// if reloads home page when going to different url, then home and after that clicking back and then forward
+		[Fact]
+		[Trait("Category", "Smoke")]
+		public void ReloadHomePageOnForward()
+		{
+			using (IWebDriver driver = new ChromeDriver())
+			{
+				driver.Navigate().GoToUrl(AboutUrl);
+				TestHelper.Pause();
+
+				driver.Navigate().GoToUrl(HomeUrl);
+				TestHelper.Pause();
+
+				driver.Navigate().Back();
+				TestHelper.Pause();
+
+				driver.Navigate().Forward();
+				TestHelper.Pause();
+
+				Assert.Equal(HomeUrl, driver.Url);
+				Assert.Equal(HomeTitle, driver.Title);
 			}
 		}
 	}
